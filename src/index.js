44c2f06edb471a1c34d8f04c8caffd413df6b559
unknown <esinamseraphine7@gmail>
2024-12-13 04:13:57 -0800

    adding all files

---
+++ src/index.js
@@ -0,0 +1,22 @@
+let username=prompt("what is your name?");
+
+let age=prompt("how old are you?");
+
+let email=prompt("what is your email address?");
+
+if(age < 12 ){
+    alert(`Hi ${name}, you are ${age} years old and are too yong to register.Sorry😔😔`)
+}else if(age > 12 && age < 18){
+    alert(`Hi ${username},you are ${age},years old and have limited options to register for.
+        we will keep in touch via ${email},👋🏾👋🏾.`)
+    }else{
+        alert(`Hi ${name},you are ${age}years old and you can register for any option of your choosing.we will keep intouch
+            via${email}👋🏾👋🏾.` )
+    } 
+    
+
+
+
+
+    
+
